f98af3dedaf2b37c28f9c2ea19fa9379e3f7bf89
Venkata naresh suddula <60736104+suddulavenkatanaresh@users.noreply.github.com>
2021-02-26 16:46:02 +0530

    Update applying _torch_trans.py

--- applying _torch_trans.py
+++ applying _torch_trans.py
@@ -7,10 +7,14 @@ from PIL  import Image
 im=Image.open('/home/vihal_venkat/Desktop/car.jpeg')
 
 
+
+
+# i want to aplly the various transformations like resize,centercrop,grayscale etc on image 
+
 transforms=transforms.Compose([  transforms.Resize(250), 
                                transforms.CenterCrop(224),
-                               transforms.Grayscale(num_output_channels=3),
-                               transforms.ToTensor()])
+                               transforms.Grayscale(num_output_channels=3)
+                               ])
 
 
 im=transforms(im)
@@ -18,4 +22,4 @@ im=transforms(im)
 
 
 im.show()
-
+im.save('result.jpg')
